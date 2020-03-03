f743e2cf41ef24f5168c5f814593d511ff80f288
Wiktor Morawski <morwiko@gmail.com>
2020-03-03 11:56:24 +0100

    Fix

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { BikeStationDetailsComponent } from './components/bike-station-details/bike-station-details.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [BikeStationDetailsComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatIconModule],
 
 exports: [BikeStationDetailsComponent]
 })
